2c7b1fd20ec0432fa56e8d9e73ff8c4951ca6eaa
githubsigmod2021 <githubsigmod2021@gmail.com>
2021-04-28 22:56:44 -0500

    work

--- lc233.h
+++ lc233.h
@@ -11,32 +11,33 @@ namespace lc233 {
     class Solution {
     public:
 
-        constexpr static int RECORDS[10]{0, 1, 21, 321, 4321, 54321, 654321, 7654321, 87654321, 987654321};
+        constexpr static int RECORDS[10]{0, 1, 20, 300, 4000, 50000, 600000, 7000000, 80000000, 900000000};
         constexpr static int DIGITS[10]{1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000};
 
         int countDigitOne(int n) {
             // First get digits
-            int digits = (int) (log(n) / log(10));
-            // 1000-2401
-            // 1000-1999 1000+ 321*2
-            // 2000-2400 100 + 21*4
-            // 2400-2400
-            // 2400-2401 1 + 0*1
-            // 13
-            // 0-9  10+1*1
-            // 10-13
+            if(n == 0) {
+                return 0;
+            }
+            int digits = (int)log10(n);
             int remain = n;
             int sum = 0;
+            int count1 = 0;
             for (int i = digits; i >= 0; i--) {
+                int step_sum = 0;
                 int digit = remain / DIGITS[i];
-                if (digit > 1) {
-                    sum += DIGITS[digit];
-                } else if (digit == 1) {
-                    sum += remain + 1;
-                }
-                sum += RECORDS[digit] * digit;
-
                 remain %= DIGITS[i];
+
+                if (digit > 1) {
+                    step_sum += DIGITS[i];
+                    step_sum += digit * RECORDS[i];
+                } else if (digit == 1) {
+                    count1 += 1;
+                    step_sum += RECORDS[i];
+                    step_sum += remain + 1;
+                }
+
+                sum += step_sum;
             }
             return sum;
         }
@@ -46,6 +47,9 @@ namespace lc233 {
         Solution solution;
         assert(6 == solution.countDigitOne(13));
         assert(0 == solution.countDigitOne(0));
+        assert(12 == solution.countDigitOne(20));
+        assert(21 == solution.countDigitOne(100));
+        assert(301 == solution.countDigitOne(1000));
     }
 }
 
---
+++ lc239.h
@@ -0,0 +1,20 @@
+//
+// Created by Harper on 4/28/21.
+//
+
+#ifndef LEETCODE_239_H
+#define LEETCODE_239_H
+
+namespace lc239 {
+    class Solution {
+    public:
+        vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+
+        }
+    };
+
+    void run() {
+
+    }
+}
+#endif //LEETCODE_239_H
