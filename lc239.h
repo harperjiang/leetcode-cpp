c6f192d79052d4bd26f7039dfbae223e092cc355
harper <harperjiang@msn.com>
2021-04-30 13:13:22 -0500

    work

--- lc239.h
+++ lc239.h
@@ -13,116 +13,28 @@ using namespace std;
 
 namespace lc239 {
     class Solution {
-    private:
-        void swap(vector<int> &heap, deque<int> &seq_to_heap, deque<int> &value_to_idx, int a, int b) {
-            int temp = heap[a];
-            heap[a] = heap[b];
-            heap[b] = temp;
-            temp = value_to_idx[a];
-            value_to_idx[a] = value_to_idx[b];
-            value_to_idx[b] = temp;
-            int seqa = value_to_idx[a];
-            int seqb = value_to_idx[b];
-            temp = seq_to_heap[seqa];
-            seq_to_heap[seqa] = seq_to_heap[seqb];
-            seq_to_heap[seqb] = temp;
-        }
-
-        void rotate(deque<int>& seq_to_heap, deque<int>&value_to_idx) {
-            int poped = seq_to_heap.front();
-            seq_to_heap.pop_front();
-            seq_to_heap.push_back(poped);
-            poped = value_to_idx.back();
-            value_to_idx.pop_back();
-            value_to_idx.push_front(poped);
-        }
-
-        void heapify(vector<int> &heap, deque<int> &seq_to_heap, deque<int> &value_to_idx) {
-            int length = heap.size();
-            for (int j = length - 1; j > 0; j--) {
-                heap_adjust(heap, seq_to_heap, value_to_idx, j);
-            }
-        }
-
-        void heap_adjust(vector<int> &heap, deque<int> &seq_to_heap, deque<int> &value_to_idx, int index) {
-            int p = index;
-            while (p > 0 && heap[(p - 1) / 2] < heap[p]) {
-                swap(heap, seq_to_heap, value_to_idx, p, (p - 1) / 2);
-                p = (p - 1) / 2;
-            }
-        }
-
-        void heapdown_adjust(vector<int> &heap, deque<int> &seq_to_heap, deque<int> &value_to_idx, int index) {
-            int length = heap.size();
-            int p = index;
-            while (true) {
-                int left = p * 2 + 1 < length ? heap[p * 2 + 1] : INT32_MIN;
-                int right = p * 2 + 2 < length ? heap[p * 2 + 2] : INT32_MIN;
-                int max = std::max(left, right);
-                if (heap[p] < max) {
-                    if (left == max) {
-                        swap(heap, seq_to_heap, value_to_idx, p, p * 2 + 1);
-                        p = p * 2 + 1;
-                    } else {
-                        swap(heap, seq_to_heap, value_to_idx, p, p * 2 + 2);
-                        p = p * 2 + 2;
-                    }
-                } else {
-                    break;
-                }
-            }
-        }
-
-        void replace_adjust(vector<int> &heap, deque<int> &seq_to_heap, deque<int> &value_to_idx, int index) {
-            int length = heap.size();
-            if (index > 0 && heap[index] > heap[(index - 1) / 2]) {
-                // Adjust up
-                heap_adjust(heap, seq_to_heap, value_to_idx, index);
-            } else {
-                heapdown_adjust(heap, seq_to_heap, value_to_idx, index);
-            }
-        }
-
     public:
-        vector<int> maxSlidingWindow(vector<int> &nums, int k) {
+        vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+            vector<int> ans;
+            deque<int> de;
+            int i=0,j=0;
 
-            vector<int> result;
+            while(j<nums.size())
+            {
+                while(!de.empty() && de.back()<nums[j])
+                    de.pop_back();
+                de.push_back(nums[j]);
 
-            if (nums.size() <= k) {
-                int max_value = INT32_MIN;
-                for (auto a: nums) {
-                    max_value = std::max(max_value, a);
+                if(j-i+1<k) j++;
+                else{
+                    ans.push_back(de.front());
+                    if(nums[i] == de.front())
+                        de.pop_front();
+                    i++;
+                    j++;
                 }
-                result.push_back(max_value);
-                return result;
             }
-
-            // max_heap[i] is the position of element in the heap
-            deque<int> seq_to_heap;
-            deque<int> value_to_idx;
-            vector<int> heap;
-            heap.resize(k);
-            value_to_idx.resize(k);
-
-            int i;
-            for (i = 0; i < k; ++i) {
-                heap[i] = nums[i];
-                seq_to_heap.push_back(i);
-                value_to_idx[i] = i;
-            }
-            heapify(heap, seq_to_heap, value_to_idx);
-
-            result.push_back(heap[0]);
-
-            int num_length = nums.size();
-            for (; i < num_length; ++i) {
-                int to_adjust = seq_to_heap[0];
-                heap[to_adjust] = nums[i];
-                rotate(seq_to_heap,value_to_idx);
-                replace_adjust(heap, seq_to_heap, value_to_idx, to_adjust);
-                result.push_back(heap[0]);
-            }
-            return result;
+            return ans;
         }
     };
 
