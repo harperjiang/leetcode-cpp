d49e057ea7b2b51a456a055b5baa95852802c6d5
harper <harperjiang@msn.com>
2021-05-05 18:04:51 -0500

    work

---
+++ lc297.h
@@ -0,0 +1,41 @@
+//
+// Created by harper on 5/5/21.
+//
+
+#ifndef LEETCODE_LC297_H
+#define LEETCODE_LC297_H
+
+namespace lc297 {
+    /**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+    class Codec {
+    public:
+
+        // Encodes a tree to a single string.
+        string serialize(TreeNode *root) {
+
+        }
+
+        // Decodes your encoded data to tree.
+        TreeNode *deserialize(string data) {
+
+        }
+    };
+
+// Your Codec object will be instantiated and called as such:
+// Codec ser, deser;
+// TreeNode* ans = deser.deserialize(ser.serialize(root));
+
+    void run() {
+
+    }
+
+}
+#endif //LEETCODE_LC297_H
