d84369a41fb02854ab3b868da0b3e46226d3a14b
harper <harperjiang@msn.com>
2021-05-07 09:10:41 -0500

    work

---
+++ lc315.h
@@ -0,0 +1,23 @@
+//
+// Created by harper on 5/7/21.
+//
+
+#ifndef LEETCODE_LC315_H
+#define LEETCODE_LC315_H
+
+#include <vector>
+
+using namespace std;
+
+namespace lc315 {
+    class Solution {
+    public:
+        vector<int> countSmaller(vector<int>& nums) {
+            vector<int> result(nums.size());
+
+            return result;
+        }
+    };
+}
+
+#endif //LEETCODE_LC315_H
